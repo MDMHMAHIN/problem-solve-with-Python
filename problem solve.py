f00f5e87d168cf404a4c0c3bf16e2bb9bb9993c6
MDMHMAHIN <mdmahamodulhasanmahin@gmail.com>
2025-02-19 17:58:59 +0600

    problem 3 add

--- problem solve.py
+++ problem solve.py
@@ -30,3 +30,9 @@ else:
 
 #3 WAP to check if a number is a multiple of 7 or not.
 
+x = int(input("Enter a number:"))
+if(x % 7 == 0):
+    print(x,"multiple of 7")
+
+else:
+    print(x,"not multiple of 7")
---
+++ test.py
@@ -0,0 +1,6 @@
+x = int(input("Enter a number:"))
+if(x % 7 == 0):
+    print(x,"multiple of 7")
+
+else:
+    print(x,"not multiple of 7")
